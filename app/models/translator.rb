f484bf846275a28a8506667731975c83215180eb
Josh Skeen <josh@joshskeen.com>
2013-01-12 17:46:56 -0500

    moar languages

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -1,4 +1,44 @@
 module Translator
-  LANGUAGES = [:de, :he, :cs, :ar, :sv, :uk, :et, :fr, :it, :tr, :fi, :ja, :ru, :sk, :id].freeze
+
+    LANGUAGE_LIST = {
+     ar: "Arabic",
+     ja: "Japanese",
+     bg: "Bulgarian",
+     ko: "Korean",
+     ca: "Catalan",
+     lv: "Latvian",
+     lt: "Lithuanian",
+     no: "Norwegian",
+     cs: "Czech",
+     fa: "Persian",
+     da: "Danish",
+     pl: "Polish",
+     nl: "Dutch",
+     pt: "Portuguese",
+     en: "English",
+     ro: "Romanian",
+     et: "Estonian",
+     ru: "Russian",
+     fi: "Finnish",
+     sk: "Slovak",
+     fr: "French",
+     sl: "Slovenian",
+     de: "German",
+     es: "Spanish",
+     el: "Greek",
+     sv: "Swedish",
+     ht: "Haitian Creole",
+     th: "Thai",
+     he: "Hebrew",
+     tr: "Turkish",
+     hi: "Hindi",
+     uk: "Ukrainian",
+     mww: "Hmong Daw",
+     vi: "Vietnamese",
+     hu: "Hungarian"
+    }.freeze
+
+  LANGUAGES = LANGUAGE_LIST.keys
   NUM_STEPS = 20
+
 end
