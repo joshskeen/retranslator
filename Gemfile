4ecd4631b290889f961622451e40b6035592131e
Josh Skeen <josh@joshskeen.com>
2014-05-15 18:13:55 -0400

    remove sqlite gem and use remote pg wordnet db

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem 'twitter'
 gem 'kaminari'
 gem 'bitly'
 gem 'wordnet'
-gem 'wordnet-defaultdb'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/translator/wordnet_mutator.rb
+++ app/models/translator/wordnet_mutator.rb
@@ -2,7 +2,7 @@ module Translator
   class WordnetMutator
 
     def initialize(args={})
-      @lexicon = WordNet::Lexicon.new
+      @lexicon = WordNet::Lexicon.new(ENV['WORDNET_CONNECTION_URL'])
       @phrase = args.fetch(:phrase_to_mutate, "")
     end
 
